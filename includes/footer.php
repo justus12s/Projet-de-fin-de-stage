d74eaa404d897097dd8e415e7fd01c36c9efc776
Abel KPOKOUTA <kpokoutaabel@gmail.com>
2025-09-10 09:28:18 +0100

    ee

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- footer -->
-<footer>
-    <div class="container">
-        <p class="mb-1">&copy; 2025 <strong>Bibliothèque DJAB Excellence</strong>. Tous droits réservés.</p>
-        <p class="mb-0">Un projet de fin de stage réalisé avec passion</p>
-    </div>
-</footer>
